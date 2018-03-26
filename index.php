2235f4d3b0757771173e6a5717e7ebbbca6898b0
Anderson Silva Brino <brino7@hotmail.com>
2018-03-26 12:23:47 -0300

    Meus primeiros arquivos php.

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <div>
+            <?php
+            $nome = "Maria";
+            $idade = 18;
+            echo "$nome tem $idade anos!";
+            ?>
+        </div>
+    </body>
+</html>
