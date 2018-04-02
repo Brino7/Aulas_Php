790d8b84999eb392f99331cc60757b2af5b7710a
Anderson Silva Brino <brino7@hotmail.com>
2018-04-02 12:22:03 -0300

    pagina que contem as variaveis da varialvel do php.

---
+++ variaveisdevariaveis.php
@@ -0,0 +1,24 @@
+<!--
+* @ Autor: Anderson Silva Brino
+* @ Data: 02/04/2018
+* @ Hora: 12:06:18
+-->
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Curso de PHP - by Gustavo Guanabara</title>
+    </head>
+    <body>
+        <?php
+        /* @var $x type */
+        $x = abc;
+            $$x = def;
+// Nas duas linhas acima esta descrito a varialvel da variavel.
+            echo "O conteudo da variavel X e $x";
+            echo "<br>O conteudo da variavel ABC criada e $abc";
+        ?>
+    </body>
+</html>
+
+
