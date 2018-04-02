76eb9849ac450f1a5a5b404ae8320ca436daa4d5
Anderson Silva Brino <brino7@hotmail.com>
2018-04-02 12:19:24 -0300

    pagina que contem as incremento do php.

---
+++ incremento.php
@@ -0,0 +1,20 @@
+<!--
+* @ Autor: Anderson Silva Brino
+* @ Data: 02/04/2018
+* @ Hora: 11:40:14
+-->
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Curso de PHP - by Gustavo Guanabara</title>
+    </head>
+    <body>
+        <?php
+            $atual = $_GET["aa"];
+            echo "O ano atual e $atual e o ano anterior e ". --$atual;
+        ?>
+    </body>
+</html>
+
+
