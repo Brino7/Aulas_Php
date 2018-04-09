175b98939670134f0c8d8be9d1f4c4032abfa9a9
Anderson Silva Brino <brino7@hotmail.com>
2018-04-09 10:56:41 -0300

    pagina que contem como usar os tres simbolos de igualdade do php.

---
+++ igualident.php
@@ -0,0 +1,26 @@
+<!--
+* @ Autor: Anderson Silva Brino
+* @ Data: 09/04/2018
+* @ Hora: 09:37:09
+-->
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+            $a = 3;
+            $b = "3";
+            /* para fazer uma comparação entre variaveis de
+            forma que elas sejam identicas usase o acrescimo de
+            mais um sinal de igualdade.
+            */
+            $r = ($a === $b)?"SIM":"NAO";
+            echo "As variaveis sao iguais? $r";
+        ?>
+    </body>
+</html>
+
+
