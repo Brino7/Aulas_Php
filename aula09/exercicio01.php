d22e0eba3fd87193ec03e3cde4194fa55c6c6e4f
Anderson Silva Brino <brino7@hotmail.com>
2018-04-30 08:49:20 -0300

    exercicio com condicional if

---
+++ aula09/exercicio01.php
@@ -0,0 +1,27 @@
+<!--
+* @ Autor: Anderson Silva Brino
+* @ Data: 23/04/2018
+* @ Hora: 07:21:35
+-->
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+            $a = isset($_GET["ano"])?$_GET["ano"]:1900;
+            $i = date("Y") - $a;
+            echo "Voce nasceu em $a e terá $i anos.<br>";
+            if ($i >= 18){
+                $v = "ja pode votar";
+                $d = "ja pode dirigir";
+            }else{
+                $v = "nao pode votar";
+                $d = "nao pode dirigir";
+            }
+            echo "Com essa idade você $v e também $d.";
+        ?>
+    </body>
+</html>
