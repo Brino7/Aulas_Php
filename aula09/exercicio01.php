1b664448d94d218aaffa4b072a1f0b344ca9dd8c
Anderson Silva Brino <brino7@hotmail.com>
2018-04-30 10:27:24 -0300

    Modificados no dia 30/04/2018

--- aula09/exercicio01.php
+++ aula09/exercicio01.php
@@ -11,17 +11,18 @@
     </head>
     <body>
         <?php
-            $a = isset($_GET["ano"])?$_GET["ano"]:1900;
-            $i = date("Y") - $a;
-            echo "Voce nasceu em $a e terá $i anos.<br>";
-            if ($i >= 18){
-                $v = "ja pode votar";
-                $d = "ja pode dirigir";
-            }else{
-                $v = "nao pode votar";
-                $d = "nao pode dirigir";
-            }
-            echo "Com essa idade você $v e também $d.";
+        $a = isset($_GET["ano"]) ? $_GET["ano"] : 1900;
+        $i = date("Y") - $a;
+        echo "Voce nasceu em $a e terá $i anos.<br>";
+        if ($i >= 18) {
+            $v = "ja pode votar";
+            $d = "ja pode dirigir";
+        } else {
+            $v = "nao pode votar";
+            $d = "nao pode dirigir";
+        }
+        echo "Com essa idade você $v e também $d.<br>";
         ?>
+        <a href="exercicio01.html">Voltar</a>
     </body>
 </html>
