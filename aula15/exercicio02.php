1f52a5603cdb7d868c42db5bb805350581aee948
brino7 <brino7@hotmail.com>
2018-05-21 12:52:49 -0300

    funcoes com referencia

---
+++ aula15/exercicio02.php
@@ -0,0 +1,23 @@
+<!--
+* @Autor: Anderson Silva Brino
+* @Data: 21/05/2018
+* @Hora: 11:26:18
+-->
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <div>
+            <?php
+            include "funcoes.php";
+            echo "<h1>Testando novas funcoes</h1>";
+            ola();
+            mostrarValor(4);
+            echo "<h2>Finalizando Programa...</h2>";
+            ?><br>    
+        </div>
+    </body>
+</html>
---
+++ aula15/funcoes.php
@@ -0,0 +1,13 @@
+<!--
+* @Autor: Anderson Silva Brino
+* @Data: 21/05/2018
+* @Hora: 11:28:36
+-->
+<?php 
+function ola(){
+    echo "<h1>Ola, Mundo!</h1>";
+}
+function mostrarValor($v){
+    echo "<h2>Acabei de receber o valor $v</h2>";
+}
+?>
